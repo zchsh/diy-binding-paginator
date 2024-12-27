d8aa34b74cc7f328ef53fe495ca8fbc267ebcbc9
Zach Shilton <4624598+zchsh@users.noreply.github.com>
2024-12-26 20:36:02 -0500

    chip away at pseudo-code

--- pdf_diy_binding_paginator.py
+++ pdf_diy_binding_paginator.py
@@ -2,7 +2,6 @@ from pypdf import PdfReader, PdfWriter, PageObject
 from pdf_is_consistent_size import pdfIsConsistentSize
 from pdf_get_diptych_fit import pdfGetDiptychFit
 
-
 # Define the input file path. This is PDF we'll be transforming for binding.
 inputFilePath = "fixtures/2024-12-05-sixteen-pages.pdf"
 
@@ -32,9 +31,33 @@ if x1 is None or y1 is None or x2 is None or y2 is None:
 # Read the input PDF
 reader = PdfReader(inputFilePath)
 
+# Get the count of pages in the input PDF
+pageCount = len(reader.pages)
+
+if pageCount % 4 != 0:
+    print("ERROR: page count needs to be divisible by 4.")
+    print("TODO: learn how to actually throw an error... for now, whatever.")
+
+#
+# PSEUDO-CODE
+#
+# Use the page count to get folio specifications
+# runningTotalPages = 0
+# signatureSpecs = getSignatures(pageCount)
+# folioSpecs = []
+# for signaturePageCount of signatureSpecs:
+#   folioSpec = getFolios(signaturePageCount)
+#   withOffset = offsetPages(folioSpecs, runningTotalPages)
+#   folioSpecs.append(folioSpec)
+#   runningTotalPages += signaturePageCount
+# 
+
 # Initialize the output PDF
 merger = PdfWriter()
 
+#
+# TODO: split out a separate function, makeDiptychPage, or something
+# Could use it here for the demo, and re-use for the "real" code to write.
 #
 # Add an example diptych page to the output PDF.
 # Later, we'll want to loop through all pages in the input PDF,
@@ -43,7 +66,7 @@ merger = PdfWriter()
 # folios, which can be assembled into signatures, and bound into a book.
 #
 # But for now, we make a couple demo pages from the first four input pages.
-#
+# 
 # Create a new page with the target size
 demoPage = PageObject.create_blank_page(width=targetSize["width"], height=targetSize["height"])
 # Merge the left page onto the new page
@@ -59,25 +82,9 @@ demoPage2.merge_translated_page(reader.pages[3], x2, y2)
 merger.add_page(demoPage2)
 
 #
-# PSEUDO CODE TO IMPLEMENT
+# PSEUDO-CODE
 #
-# # Get the count of pages in the input PDF
-# pageCount = len(reader.pages)
-# # TODO: could maybe throw a helpful error here if pageCount % 4 != 0
-#
-# # USE THE PAGE COUNT TO BUILD FOLIO SPECS
-# 
-# runningTotalPages = 0
-# signatureSpecs = getSignatures(pageCount)
-# folioSpecs = []
-# for signaturePageCount of signatureSpecs:
-#   folioSpec = getFolios(signaturePageCount)
-#   withOffset = offsetPages(folioSpecs, runningTotalPages)
-#   folioSpecs.append(folioSpec)
-#   runningTotalPages += signaturePageCount
-# 
-# # USE THE FOLIO SPECS TO BUILD THE OUTPUT PDF
-#
+# Use the folios specs to build the output PDF
 # for folioSpec of folioSpecs:
 #   for folioSide of folioSpec:
 #      leftPageIndex, rightPageIndex = folioSide
